748b7df84b1ae0e41244e7a990fe2a3f20e672db
irmalop <ada_wit18@hackademy.mx>
2022-09-18 21:11:45 -0500

    Agregamos configuración para postgresql

--- lugares_seguros/lugares_seguros/settings/base.py
+++ lugares_seguros/lugares_seguros/settings/base.py
@@ -79,13 +79,23 @@ WSGI_APPLICATION = 'lugares_seguros.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd5ebgm1hsmkbi',
+        'USER': 'deoxjjgyyffsip',
+        'HOST': 'ec2-34-200-205-45.compute-1.amazonaws.com',
+        'PASSWORD': '476a9b4d619cb1d046c18ff7ae6874d369c5a6dcda250de92c02c3c85b614c29',
+        'PORT': 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
